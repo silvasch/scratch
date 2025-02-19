243574892c9cd5f6f4f3233123917aa72127a337
Silvan Schmidt <mail@silvanschmidt.ch>
2025-02-19 15:27:35 +0100

    refactor: extract code to functions

--- src/main.c
+++ src/main.c
@@ -12,7 +12,31 @@
   #define TMP_DIR ""
 #endif
 
-#define TMP_DIR_LEN sizeof(TMP_DIR)
+#define FILE_PATH_LEN sizeof(TMP_DIR) + UUID_STR_LEN + 3
+
+char *get_editor() {
+  char* editor = getenv("EDITOR");
+  if (editor == NULL) {
+    editor = "nano";
+  }
+  return editor;
+}
+
+char *get_uuid() {
+  uuid_t binuuid;
+  uuid_generate_random(binuuid);
+  char *uuid = malloc(UUID_STR_LEN);
+  uuid_unparse_lower(binuuid, uuid);
+  return uuid;
+}
+
+void run_editor(char *editor, char file_path[FILE_PATH_LEN]) {
+    char *args[3];
+    args[0] = editor;
+    args[1] = file_path;
+    args[2] = NULL;
+    execvp(editor, args);
+}
 
 int main(int argc, char *argv[]) {
   if (strcmp(TMP_DIR, "") == 0) {
@@ -20,17 +44,10 @@ int main(int argc, char *argv[]) {
     return 1;
   }
   
-  char *editor = getenv("EDITOR");
-  if (editor == NULL) {
-    editor = "nano";
-  }
-
-  uuid_t binuuid;
-  uuid_generate_random(binuuid);
-  char *uuid = malloc(UUID_STR_LEN);
-  uuid_unparse_lower(binuuid, uuid);
-
-  char file_path[TMP_DIR_LEN + UUID_STR_LEN + 3];
+  char *editor = get_editor();
+  char *uuid = get_uuid();
+   
+  char file_path[FILE_PATH_LEN];
   sprintf(file_path, "%s%s.md", TMP_DIR, uuid);
 
   printf("creating scratch file in '%s'.\n", file_path);
@@ -42,12 +59,7 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "forking failed\n");
     return 1;
   } else if (pid == 0) {
-    // char *args[] = { file_path, NULL };
-    char *args[3];
-    args[0] = editor;
-    args[1] = file_path;
-    args[2] = NULL;
-    execvp(editor, args);
+    run_editor(editor, file_path);
   } else {
     wait(NULL);
   }
